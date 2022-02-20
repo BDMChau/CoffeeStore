92b42bd2b39d331b22efbf52df3c215e56c5bd8b
MChau105 <bdmchau10005@gmail.com>
2022-02-20 17:26:17 +0700

    configuring jpa

--- src/main/java/com/coffeestore/config/ApplicationConfig.java
+++ src/main/java/com/coffeestore/config/ApplicationConfig.java
@@ -1,9 +1,17 @@
 package com.coffeestore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 
 @Configuration
@@ -12,38 +20,45 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ApplicationConfig implements WebMvcConfigurer {
     // this file is like application.properties in Spring Boot
 
-//    @Bean(name = "sessionFactory")
-//    public LocalSessionFactoryBean sessionFactory() {
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-////        sessionFactory.setDataSource(dataSource());
-//        sessionFactory.setPackagesToScan("com.coffeestore.model");
-//        sessionFactory.setHibernateProperties(hibernateProperties());
-//        return sessionFactory;
-//    }
-//
-//    @Bean(name = "dataSource")
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//        dataSource.setUrl("jdbc:mysql://localhost:3306/ecommerce?serverTimezone=UTC");
-//        dataSource.setUsername("root");
-//        dataSource.setPassword("root");
-//
-//        return dataSource;
-//    }
-//
-//    @Bean
-//    public MultipartResolver multipartResolver() {
-//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-//        multipartResolver.setMaxUploadSize(10240000);
-//        return multipartResolver;
-//    }
-//
-//    /////////////////// private methods ///////////////////
-//    private final Properties hibernateProperties() {
-//        Properties hibernateProperties = new Properties();
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.coffeestore.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/CoffeeStoreDb");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1");
+
+        return dataSource;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(10240000);
+        return multipartResolver;
+    }
+
+    /////////////////// private methods ///////////////////
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
 //        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-//        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-//        return hibernateProperties;
-//    }
+//        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+        hibernateProperties.setProperty("spring.jpa.generate-ddl", "true");
+        hibernateProperties.setProperty("spring.jpa.hibernate.ddl-auto", "update");
+        hibernateProperties.setProperty("spring.jpa.open-in-view", "false");
+        hibernateProperties.setProperty("spring.jpa.show_sql", "true");
+        hibernateProperties.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+        return hibernateProperties;
+    }
 }
---
+++ src/main/java/com/coffeestore/config/JpaConfig.java
@@ -0,0 +1,46 @@
+package com.coffeestore.config;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.coffeestore.repository"})
+@EnableTransactionManagement
+public class JpaConfig {
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+        factoryBean.setPersistenceUnitName("CoffeeStoreDb");
+
+        return factoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        transactionManager.setDataSource(datasourceConfig());
+
+        return transactionManager;
+    }
+
+
+
+    //////////// private ///////////
+    private DriverManagerDataSource datasourceConfig(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/CoffeeStoreDb");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1");
+
+        return dataSource;
+    }
+}
