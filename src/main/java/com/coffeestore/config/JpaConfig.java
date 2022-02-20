f76cffd652b330e677f38b83557a4f0bd46bb8cf
MChau105 <bdmchau10005@gmail.com>
2022-02-21 00:12:20 +0700

    .

--- src/main/java/com/coffeestore/config/JpaConfig.java
+++ src/main/java/com/coffeestore/config/JpaConfig.java
@@ -1,14 +1,14 @@
 package com.coffeestore.config;
 
-import javax.persistence.EntityManagerFactory;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.coffeestore.repository"})
@@ -16,31 +16,19 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JpaConfig {
     @Bean
     public LocalEntityManagerFactoryBean entityManagerFactory() {
-        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
-        factoryBean.setPersistenceUnitName("CoffeeStoreDb");
+        LocalEntityManagerFactoryBean localEntityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        localEntityManagerFactoryBean.setPersistenceUnitName("CoffeeStoreDb");
 
-        return factoryBean;
+        return localEntityManagerFactoryBean;
     }
 
     @Bean
     public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        transactionManager.setDataSource(datasourceConfig());
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
 
-        return transactionManager;
+        return jpaTransactionManager;
     }
 
 
-
-    //////////// private ///////////
-    private DriverManagerDataSource datasourceConfig(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/CoffeeStoreDb");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("1");
-
-        return dataSource;
-    }
 }
