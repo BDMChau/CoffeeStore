6e2a42e12317a01c15a466ea5fe28e7a8c587ef8
Gitopham1356 <75571286+Gitopham1356@users.noreply.github.com>
2022-02-21 21:00:04 +0700

    update_testing

--- src/main/java/com/coffeestore/model/Customer.java
+++ src/main/java/com/coffeestore/model/Customer.java
@@ -1,27 +1,54 @@
 package com.coffeestore.model;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Collection;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "customer")
 @Entity
 @Data
-public class Customer implements Serializable {
+public class Customer  {
 
-    private static final long serialVersionUID = 1L;
+  //  private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
-    private Long id;
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_sequence" // same as NAME in SequenceGenerator
+    )
+    private Long Id;
 
-    @Column
+//    @JsonBackReference
+//    @OneToMany(mappedBy = "coinUsageWay", cascade = CascadeType.ALL)
+//    private Collection<CoinUsageWay> coinUsageWays;
+
+//    @ManyToMany(mappedBy = "genre") // variable genre in manga class
+//    private Collection<MangaGenre> mangaGenre;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
     private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date_of_birth")
+    private Calendar date_of_Birth;
+
+    @Column(name = "testing", nullable = false)
+    private String testing;
 
 }
