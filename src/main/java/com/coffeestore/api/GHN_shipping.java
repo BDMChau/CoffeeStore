403cebf09be8c21ee56d694356a78d4b8752180d
MChau105 <bdmchau10005@gmail.com>
2022-03-05 18:06:49 +0700

    .

---
+++ src/main/java/com/coffeestore/api/GHN_shipping.java
@@ -0,0 +1,9 @@
+package com.coffeestore.api;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class GHN_shipping {
+    private static String token = "28014046-9c3d-11ec-913f-a2241d5a8154";
+}
---
+++ src/main/java/com/coffeestore/api/MOMO_transaction.java
@@ -0,0 +1,11 @@
+package com.coffeestore.api;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MOMO_transaction {
+
+    private static String partnerCode = "MOMOEKPV20220305";
+
+}
