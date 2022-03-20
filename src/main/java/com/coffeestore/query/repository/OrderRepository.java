a759432f810341224a4195f6a8142f4c0599be28
Gitopham1356 <75571286+Gitopham1356@users.noreply.github.com>
2022-03-20 09:22:42 +0700

    push

--- src/main/java/com/coffeestore/query/repository/OrderRepository.java
+++ src/main/java/com/coffeestore/query/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import com.coffeestore.model.order.Orders;
 import com.coffeestore.query.dto.OrderDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
@@ -14,15 +15,15 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Orders, Long> {
 
     @Query("SELECT new com.coffeestore.query.dto.OrderDto("+
-           " or.id, or.created_at, or.total_bill " +
+           " orders.id, orders.created_at, orders.total_bill " +
            ", ord.id, ord.product_quantity " +
            ", pr.id, pr.name, pr.price) " +
-           "FROM Orders or " +
-           "JOIN User u ON u.id = or.user.id " +
-           "LEFT JOIN OrderDetail ord ON ord.Orders.id = or.id " +
+           "FROM Orders orders " +
+           "JOIN User u ON u.id = orders.user.id " +
+           "LEFT JOIN OrderDetail ord ON ord.Orders.id = orders.id " +
            "LEFT JOIN Product pr ON pr.id = ord.product.id " +
-           "WHERE u.email =?1 " +
-           "ORDER BY or.created_at")
-    List<OrderDto> getUserOrders(Pageable pageable, String user_email);
+           "WHERE u.email = (:user_email) " +
+           "ORDER BY orders.created_at")
+    List<OrderDto> getUserOrders(Pageable pageable, @Param("user_email") String user_email);
 
 }
