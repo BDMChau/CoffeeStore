bd1db31c521738e05a5280635f00f148dcfe8503
Gitopham1356 <75571286+Gitopham1356@users.noreply.github.com>
2022-03-27 21:12:51 +0700

    push

--- src/main/java/com/coffeestore/service/user/UserService.java
+++ src/main/java/com/coffeestore/service/user/UserService.java
@@ -14,120 +14,148 @@ import java.util.*;
 
 @Service
 public class UserService {
-    private String avatarDefault = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png";
+  private String avatarDefault = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png";
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private AddressRepo addressRepo;
+  @Autowired
+  private AddressRepo addressRepo;
 
-    @Autowired
-    private RoleRepository roleRepository;
+  @Autowired
+  private RoleRepository roleRepository;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+  @Autowired
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public boolean register(User user) {
-        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
-        if(userOptional.isPresent()) return false;
+  public boolean register(User user) {
+    Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+    if (userOptional.isPresent()) return false;
 
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setName(user.getName());
-        user.setEmail(user.getEmail());
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    user.setName(user.getName());
+    user.setEmail(user.getEmail());
 //        user.setPhone();
 //        user.setAddresses();
-        user.setAvatar(avatarDefault);
+    user.setAvatar(avatarDefault);
 //        user.setBirthday();
 //        user.setGender();
-        user.setCreated_at(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
+    user.setCreated_at(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
 
-        Role role = roleRepository.getOne(1L); // role: USER
-        user.setRole(role);
+    Role role = roleRepository.getOne(1L); // role: USER
+    user.setRole(role);
 
-        userRepository.saveAndFlush(user);
-        return true;
-    }
+    userRepository.saveAndFlush(user);
+    return true;
+  }
 
 
-
-    public boolean updateProfile(User userInfo){
-        User user = getUserByEmail(userInfo.getEmail());
-        if(user == null) return false;
+  public boolean updateProfile(User userInfo) {
+    User user = getUserByEmail(userInfo.getEmail());
+    if (user == null) return false;
 
 //        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setName(userInfo.getName());
+    user.setName(userInfo.getName());
 //        user.setEmail(userInfo.getEmail());
-        user.setPhone(userInfo.getPhone());
-        user.setAddresses(userInfo.getAddresses());
+    user.setPhone(userInfo.getPhone());
+    user.setAddresses(userInfo.getAddresses());
 //        user.setAvatar(avatarDefault);
-        user.setBirthday(userInfo.getBirthday());
+    user.setBirthday(userInfo.getBirthday());
 //        user.setGender();
 //        user.setCreated_at(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
 
 //        Role role = roleRepository.getOne(1L); // role: USER
 //        user.setRole(role);
 
-        userRepository.saveAndFlush(user);
+    userRepository.saveAndFlush(user);
 
-        return true;
+    return true;
+  }
+
+  public Map updateAddress(Map data, String userEmail) {
+    User user = getUserInfo(userEmail);
+
+    List<Address> addresses = addressRepo.getAddressByUserId(user.getId());
+    boolean isMain = false;
+    if (addresses.isEmpty()) {
+      isMain = true;
+    }
+    Address address = new Address();
+    address.setUser(user);
+    address.setCity_province(String.valueOf(data.get("city")));
+    address.setDistrict(String.valueOf(data.get("district")));
+    address.setWard(String.valueOf(data.get("ward")));
+    address.setAddress(String.valueOf(data.get("detail")));
+    address.setIs_main(isMain);
+
+    addressRepo.saveAndFlush(address);
+
+    Map map = new HashMap();
+    map.put("city", address.getCity_province());
+    map.put("district", address.getDistrict());
+    map.put("ward", address.getWard());
+    map.put("user_id", address.getUser().getId());
+
+    return map;
+  }
+
+  public Boolean deleteAddress(Map data, String userEmail) {
+    User user = getUserInfo(userEmail);
+    Long targetId = Long.parseLong(String.valueOf(data.get("address_id")));
+    Optional<Address> addressOptional = addressRepo.findById(targetId);
+    List<Address> addressList = addressRepo.getAddressByUserId(user.getId());
+    if (addressList.size() == 1) {
+      return false;
     }
 
-    public Map updateAddress(Map data, String userEmail){
-        User user = getUserInfo(userEmail);
+    if (addressOptional.isPresent()) {
+      addressRepo.delete(addressOptional.get());
 
-        Address address = new Address();
-        address.setUser(user);
-        address.setCity_province(String.valueOf(data.get("city")));
-        address.setDistrict(String.valueOf(data.get("district")));
-        address.setWard(String.valueOf(data.get("ward")));
-        address.setAddress(String.valueOf(data.get("detail")));
-
+      List<Address> addresses = addressRepo.getAddressByUserId(user.getId());
+      if (addresses.size() == 1) {
+        Address address = addresses.get(0);
+        address.setIs_main(true);
         addressRepo.saveAndFlush(address);
-
-        Map map = new HashMap();
-        map.put("city", address.getCity_province());
-        map.put("district", address.getDistrict());
-        map.put("ward", address.getWard());
-        map.put("user_id", address.getUser().getId());
-
-        return map;
+      }
+      return true;
     }
 
-
-    public Map updateDefaultAddress(Map data, String userEmail) {
-        User user = getUserInfo(userEmail);
-        Long targetId = Long.parseLong(String.valueOf(data.get("address_id")));
-
-        Map map = new HashMap();
-
-        List<Address> addresses = addressRepo.getAddressByUserId(user.getId());
-        addresses.forEach(address -> {
-            if(address.getId().equals(targetId)) {
-                address.setIs_main(true);
-                map.put("address_id", address.getId());
-            }
-            else address.setIs_main(false);
-
-            addressRepo.saveAndFlush(address);
-        });
+    return false;
+  }
 
 
-        return map;
-    }
+  public Map updateDefaultAddress(Map data, String userEmail) {
+    User user = getUserInfo(userEmail);
+    Long targetId = Long.parseLong(String.valueOf(data.get("address_id")));
+
+    Map map = new HashMap();
+
+    List<Address> addresses = addressRepo.getAddressByUserId(user.getId());
+    addresses.forEach(address -> {
+      if (address.getId().equals(targetId)) {
+        address.setIs_main(true);
+        map.put("address_id", address.getId());
+      } else address.setIs_main(false);
+
+      addressRepo.saveAndFlush(address);
+    });
 
 
-    public User getUserInfo(String userEmail){
-        Optional<User> userOptional = userRepository.findByEmail(userEmail);
-        return userOptional.orElseGet(User::new);
-    }
+    return map;
+  }
 
 
-    public User getUserByEmail(String email) {
-        Optional<User> userOptional = userRepository.findByEmail(email);
-        if(!userOptional.isPresent()) return null;
+  public User getUserInfo(String userEmail) {
+    Optional<User> userOptional = userRepository.findByEmail(userEmail);
+    return userOptional.orElseGet(User::new);
+  }
 
-        return userOptional.get();
-    }
+
+  public User getUserByEmail(String email) {
+    Optional<User> userOptional = userRepository.findByEmail(email);
+    if (!userOptional.isPresent()) return null;
+
+    return userOptional.get();
+  }
 
 }
